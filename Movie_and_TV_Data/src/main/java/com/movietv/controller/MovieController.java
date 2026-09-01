2e15b3e6183267da234b47e97f0b1b97930ce05e
MengF-W <123597866+MengF-W@users.noreply.github.com>
2026-09-01 16:16:12 +0200

    Code Refactor
    Refactor MovieTrailer as an independent method

--- Movie_and_TV_Data/src/main/java/com/movietv/controller/MovieController.java
+++ Movie_and_TV_Data/src/main/java/com/movietv/controller/MovieController.java
@@ -1,10 +1,14 @@
 package com.movietv.controller;
 
+import com.movietv.model.Movie;
 import com.movietv.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -16,7 +20,10 @@ public class MovieController {
     public ModelAndView viewMovie(ModelAndView model) {
 
         model.setViewName("view-movie");
-        model.addObject("movies", movieService.getMovieList());
+        List<Movie> movieList = movieService.getMovieList();
+        movieList.forEach(movie -> movie.setMovie_trailer(movieService.getMovieTrailer(movie.getId().toString())));
+
+        model.addObject("movies", movieList);
         return model;
     }
 }
--- Movie_and_TV_Data/src/main/java/com/movietv/services/MovieService.java
+++ Movie_and_TV_Data/src/main/java/com/movietv/services/MovieService.java
@@ -1,21 +1,18 @@
 package com.movietv.services;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.movietv.model.Movie;
 import com.movietv.utilities.JsonProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class MovieService {
@@ -27,32 +24,38 @@ public class MovieService {
     private String API_KEY;
     public List<Movie> getMovieList(){
 
-        final String TRENDING_MOVIE_URL = "https://api.themoviedb.org/3/trending/all/day?api_key="+API_KEY+"&language=en-US";
+        List<Movie> movieList = new ArrayList<>();
 
-        String responseResult =
-                restTemplate.getForObject(TRENDING_MOVIE_URL, String.class);
+        try{
+            final String TRENDING_MOVIE_URL = "https://api.themoviedb.org/3/trending/all/day?api_key="+API_KEY+"&language=en-US";
 
-        List<JsonElement> resultList = JsonParser.parseString(responseResult).getAsJsonObject().get("results").getAsJsonArray().asList();
-        List<Movie> movieList = new ArrayList<Movie>();
-        resultList.forEach(result -> appendMovieList(result,movieList));
+            String responseResult =
+                    restTemplate.getForObject(TRENDING_MOVIE_URL, String.class);
+
+            List<JsonElement> resultList = JsonParser.parseString(responseResult).getAsJsonObject().get("results").getAsJsonArray().asList();
+
+            resultList.forEach(result -> {
+                final String IMAGE_URL = "https://image.tmdb.org/t/p/w154";
+
+                Movie movie = JsonProcessor.getInstance().deserializeJson(result.toString(),Movie.class);
+                movie.setThumbnail_poster(IMAGE_URL+movie.getPoster_path());
+                movieList.add(movie);
+            });
+
+        }catch(HttpClientErrorException e)
+        {
+            e.printStackTrace();
+        }
 
         return movieList;
     }
 
-    private void appendMovieList(JsonElement result, List<Movie> movieList){
-        final String IMAGE_URL = "https://image.tmdb.org/t/p/w154";
 
-        Movie movie = JsonProcessor.getInstance().deserializeJson(result.toString(),Movie.class);
-        movie.setThumbnail_poster(IMAGE_URL+movie.getPoster_path());
-        setMovieTrailer(movie);
-        movieList.add(movie);
+    public String getMovieTrailer(String movieId){
 
-    }
-
-    private void setMovieTrailer(Movie movie){
-
-        final String VIDEO_URL ="https://api.themoviedb.org/3/movie/"+ movie.getId() +"/videos?api_key="+API_KEY;
+        final String VIDEO_URL ="https://api.themoviedb.org/3/movie/"+ movieId +"/videos?api_key="+API_KEY;
         final String YOUTUBE_URL ="https://www.youtube.com/embed/";
+        AtomicReference<String> MOVIE_TRAILER_URL = new AtomicReference<>("");
 
         try{
             String responseResult =
@@ -63,11 +66,12 @@ public class MovieService {
             resultList
                     .stream()
                     .filter(result -> result.getAsJsonObject().get("type").toString().contains("Trailer"))
-                    .forEach(result -> movie.setMovie_trailer(YOUTUBE_URL+result.getAsJsonObject().get("key").getAsString()));
-
+                    .forEach(result -> MOVIE_TRAILER_URL.set(YOUTUBE_URL + result.getAsJsonObject().get("key").getAsString()));
         }
         catch (HttpClientErrorException e){
-            movie.setMovie_trailer("");
+            MOVIE_TRAILER_URL.set("");
         }
+
+        return MOVIE_TRAILER_URL.get();
     }
 }
